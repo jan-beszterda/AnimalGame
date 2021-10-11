e52ad44c2579964e1b988f820e052863cc930535
Damir1975 <dkahvic@hotmail.com>
2021-10-11 11:51:15 +0200

    First version!

--- src/grupp6/djurspelet/food/Food.java
+++ src/grupp6/djurspelet/food/Food.java
@@ -1,4 +1,10 @@
 package grupp6.djurspelet.food;
 
 public abstract class Food {
+
+    public String name;
+    public int pricePerKilo;
+
+    public abstract String getName();
+    public abstract int getPricePerKilo();
 }
