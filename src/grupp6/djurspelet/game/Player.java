13c512115f9c03c39655343d55c8a4a866ea3978
jan-beszterda <jan.beszterda@gmail.com>
2021-10-11 15:18:41 +0200

    Started creating Player class. Filled with data (variables) and empty methods.

--- src/grupp6/djurspelet/game/Player.java
+++ src/grupp6/djurspelet/game/Player.java
@@ -1,4 +1,58 @@
 package grupp6.djurspelet.game;
 
+import grupp6.djurspelet.animal.Animal;
+import grupp6.djurspelet.food.Food;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class Player {
+
+    private int money;
+    private String name;
+    private ArrayList<Animal> animalsOwned;
+    private HashMap<Food, Integer> fodderOwned;
+
+    public Player(String name) {
+        this.money = 10000;
+        this.name = name;
+        this.animalsOwned = new ArrayList<>();
+        this.fodderOwned = new HashMap<>();
+    }
+
+    public void buyAnimal(Store store) {
+
+    }
+
+    public void buyFodder(Store store) {
+
+    }
+
+    public void sellAnimal() {
+
+    }
+
+    public void attemptToMateAnAnimal() {
+
+    }
+
+    public void feedAnAnimal() {
+
+    }
+
+    public int getMoney() {
+        return money;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public ArrayList<Animal> getAnimalsOwned() {
+        return animalsOwned;
+    }
+
+    public HashMap<Food, Integer> getFodderOwned() {
+        return fodderOwned;
+    }
 }
