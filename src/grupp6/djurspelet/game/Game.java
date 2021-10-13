b25fe4b5a77f776a6d25bb804b2a93b61d9938fc
jan-beszterda <jan.beszterda@gmail.com>
2021-10-13 09:23:32 +0200

    I added comments to Game class.

--- src/grupp6/djurspelet/game/Game.java
+++ src/grupp6/djurspelet/game/Game.java
@@ -1,4 +1,18 @@
 package grupp6.djurspelet.game;
 
 public class Game {
+
+    /*
+    We will need a variable to hold all players in the game, example ArrayList
+    We will need a variable to hold current player that makes choices
+    We will need a variable to hold the total number of rounds players will play
+    We will need a variable to hold the number of current round
+
+    We will need method to move the round to next player
+    We will need method to start the game
+    We will need method to save game
+    We will need method to load game
+    We will need method to quit game
+    We will need method to finalise the game
+    */
 }
