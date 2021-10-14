a2bd509a34c558f8e33f24492cad28a4e52bcba9
jan-beszterda <jan.beszterda@gmail.com>
2021-10-14 08:43:48 +0200

    I added comments to Grass and Corn classes.

--- src/grupp6/djurspelet/food/Corn.java
+++ src/grupp6/djurspelet/food/Corn.java
@@ -3,18 +3,19 @@ package grupp6.djurspelet.food;
 
 class Corn extends Food{
 
-    private int pricePerKilo;
+    private int pricePerKilo; //price will be in the Store class
     private String name;
 
-    public Corn(String name, int pricePerKilo){
+    public Corn(String name, int pricePerKilo){ //since price is in the Store class only one parameter (String name) is needed
         this.name = "Korn";
-        pricePerKilo = 50;
+        pricePerKilo = 50; //with price in the Store class this line is no longer needed
     }
 
-    public String getName() {
+    public String getName() { //this is inherited from Food class so no need to write it again here
         return name;
     }
-    public int getPricePerKilo() {
+
+    public int getPricePerKilo() { //with price in the Store class this method is no longer needed
         return pricePerKilo;
     }
 
